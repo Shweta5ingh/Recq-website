38ceb42b7f168702ef6dd5673646576ad3342ccb
Areness Consulting <Pradeep-Gupta-96>
2024-02-16 18:13:02 +0530

    css added

--- src/Components/Home Page/Home.jsx
+++ src/Components/Home Page/Home.jsx
@@ -102,7 +102,7 @@ const Home = () => {
         <Typography variant="body1" paragraph style={{ textAlign: 'center' }}>
           Sed sodales, lorem eget euismod porttitor, nisl nunc vestibulum lorem, nec aliquam ipsum justo nec quam. Integer ac tempor magna.
         </Typography>
-        {/* <div className="elementor-shape elementor-shape-bottom">
+        {/*  b<div className="elementor-shape elementor-shape-bottom">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
         <path class="elementor-shape-fill" d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7
           c23.6,2.1,48.7,3.5,103.4-2.5c54.7-6,106.2-25.6,106.2-25.6V0H0v30.3c0,0,72,32.6,158.4,30.5c39.2-0.7,92.8-6.7,134-22.4
