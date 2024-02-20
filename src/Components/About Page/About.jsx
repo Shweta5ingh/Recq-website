bbe4cf9674ac98538f2f3f29e32de1d74080c2e7
Areness Consulting <Pradeep-Gupta-96>
2024-02-20 16:56:33 +0530

    css added

--- src/Components/About Page/About.jsx
+++ src/Components/About Page/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Typography, Grid, Paper, Card, CardContent } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material';
 import Navbar from '../Common/Navbar';
 import Footer from '../Common/Footer';
 
@@ -10,51 +11,65 @@ const About = () => {
       <Navbar />
 
   
-      <Grid container spacing={2} style={{backgroundImage:'url(images/background.png)', marginTop:0}}>
-      <div className="container">
+      {/* <Grid className='page-content' container spacing={2} style={{backgroundImage:'url(images/background.png)', marginTop:0}}>
+        <div className="container">
           <div className="tile">
             <div className="main-title">About RecQarz</div>
           </div>
-
-      </div>
-      {/* First Card */}
-      <Grid className='about-info' item xs={12}>
-        <Card>
-          <CardContent>
+          <Grid className='about-info' item xs={12}>
             <Typography variant="h5">
               Recht Tech Private Limited (RECQARZ) takespride in delivering exceptional legal and consulting services in India. 
               We ensure to abide by our core competencies of startegic business advisory, seamless business establishment, and realistic everyday solutions.
             </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-
-      {/* Second Card */}
-      <Grid className='about-info' item xs={12}>
-        <Card>
-          <CardContent>
             <Typography variant="h5">
               Our unconventional ethos, client-centeredness, innovative approaches and solutions define our reputation as the leaders of our industry. 
               Considering the evolution of the 'new normal,' our cutting edge legal services outperform the traditional methodologies.
-            </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-
-      {/* Third Card */}
-      <Grid className='about-info' item xs={12}>
-        <Card>
-          <CardContent>
-            <Typography variant="h5">
+            </Typography><Typography variant="h5">
               Our mantra of Experience, Ethics, and Execellence positions as the pioneers of the industry.
             </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-    </Grid>
+          </Grid>
 
+          
+        </div>
+      </Grid> */}
+      <div className='page-content'>
+        <div className='g-section about-section'>
+          <div className="container">
+            <Grid container spacing={2}>
+              {/* Image */}
+              <Grid item xs={12} md={6}>
+                <div className="about-img">
+                  <img src="images/about-img.jpg" alt="About us" style={{ width: '100%', maxWidth: '100%', height: '100%', borderRadius: 4 }} />
+                </div>
+              </Grid>
+              {/* Text */}
+              <Grid item xs={12} md={6}>
+                <div className="about-text">
+
+                  <div style={{textAlign: 'left'}} className="main-title">About RecQarz</div>
+                  <ul style={{ listStyleType: 'none', padding: 5 }}>
+                      <li style={{ display: 'flex', alignItems: 'top'}}>
+                        <CheckCircle style={{ marginRight: '10px', color: 'black' }} />
+                        Recht Tech Private Limited (RECQARZ) takespride in delivering exceptional legal and consulting services in India. 
+                We ensure to abide by our core competencies of startegic business advisory, seamless business establishment, and realistic everyday solutions. 
+                      </li>
+                      <li style={{ display: 'flex', alignItems: 'top'}}>
+                        <CheckCircle style={{ marginRight: '10px', color: 'black' }} />
+                        Our unconventional ethos, client-centeredness, innovative approaches and solutions define our reputation as the leaders of our industry. 
+                Considering the evolution of the 'new normal,' our cutting edge legal services outperform the traditional methodologies. 
+                      </li>
+                      <li style={{ display: 'flex', alignItems: 'top'}}>
+                        <CheckCircle style={{ marginRight: '10px', color: 'black' }} />
+                        Our mantra of Experience, Ethics, and Execellence positions as the pioneers of the industry.
+                      </li>
+                    </ul>
+                </div>
+              </Grid>
+            </Grid>
+          </div>
+        </div>
+      </div>
       
-
       <Footer />
     </div>
   )
